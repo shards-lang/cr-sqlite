e98ae8f188c73356e0c4de7114ecc52b5804a19b
Giovanni Petrantoni <7008900+sinkingsugar@users.noreply.github.com>
2023-05-26 12:41:31 +0800

    comment out more stuff

--- core/rs/bundle/src/lib.rs
+++ core/rs/bundle/src/lib.rs
@@ -26,45 +26,45 @@ use sqlite_nostd::SQLite3Allocator;
 // #[lang = "eh_personality"]
 // extern "C" fn eh_personality() {}
 
-#[cfg(target_family = "wasm")]
-#[no_mangle]
-pub extern "C" fn __rust_alloc(size: usize, align: usize) -> *mut u8 {
-    unsafe { ALLOCATOR.alloc(Layout::from_size_align_unchecked(size, align)) }
-}
+// #[cfg(target_family = "wasm")]
+// #[no_mangle]
+// pub extern "C" fn __rust_alloc(size: usize, align: usize) -> *mut u8 {
+//     unsafe { ALLOCATOR.alloc(Layout::from_size_align_unchecked(size, align)) }
+// }
 
-#[cfg(target_family = "wasm")]
-#[no_mangle]
-pub extern "C" fn __rust_dealloc(ptr: *mut u8, size: usize, align: usize) {
-    unsafe { ALLOCATOR.dealloc(ptr, Layout::from_size_align_unchecked(size, align)) }
-}
+// #[cfg(target_family = "wasm")]
+// #[no_mangle]
+// pub extern "C" fn __rust_dealloc(ptr: *mut u8, size: usize, align: usize) {
+//     unsafe { ALLOCATOR.dealloc(ptr, Layout::from_size_align_unchecked(size, align)) }
+// }
 
-#[cfg(target_family = "wasm")]
-#[no_mangle]
-pub extern "C" fn __rust_realloc(
-    ptr: *mut u8,
-    old_size: usize,
-    align: usize,
-    size: usize,
-) -> *mut u8 {
-    unsafe {
-        ALLOCATOR.realloc(
-            ptr,
-            Layout::from_size_align_unchecked(old_size, align),
-            size,
-        )
-    }
-}
+// #[cfg(target_family = "wasm")]
+// #[no_mangle]
+// pub extern "C" fn __rust_realloc(
+//     ptr: *mut u8,
+//     old_size: usize,
+//     align: usize,
+//     size: usize,
+// ) -> *mut u8 {
+//     unsafe {
+//         ALLOCATOR.realloc(
+//             ptr,
+//             Layout::from_size_align_unchecked(old_size, align),
+//             size,
+//         )
+//     }
+// }
 
-#[cfg(target_family = "wasm")]
-#[no_mangle]
-pub extern "C" fn __rust_alloc_zeroed(size: usize, align: usize) -> *mut u8 {
-    unsafe { ALLOCATOR.alloc_zeroed(Layout::from_size_align_unchecked(size, align)) }
-}
+// #[cfg(target_family = "wasm")]
+// #[no_mangle]
+// pub extern "C" fn __rust_alloc_zeroed(size: usize, align: usize) -> *mut u8 {
+//     unsafe { ALLOCATOR.alloc_zeroed(Layout::from_size_align_unchecked(size, align)) }
+// }
 
-#[cfg(target_family = "wasm")]
-#[no_mangle]
-pub fn __rust_alloc_error_handler(_: Layout) -> ! {
-    core::intrinsics::abort()
+// #[cfg(target_family = "wasm")]
+// #[no_mangle]
+// pub fn __rust_alloc_error_handler(_: Layout) -> ! {
+//     core::intrinsics::abort()
 }
 
 #[no_mangle]
